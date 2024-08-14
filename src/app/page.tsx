8207bbcda358b845f2f88a8ab2aba74bef8e248e
Ahmet Burak Arslan <arslanahmet4772@gmail.com>
2024-08-15 00:55:52 +0200

    An attempt was made to add search bar functionality (there is 1 error)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,20 +2,27 @@
 //backend
 import { useState } from "react"
 import axios from "axios"
-import dotenv from 'dotenv'
 
 //frontend
 import { Input } from "@/components/ui/input"
 import { Search } from "lucide-react"
 
-dotenv.config({ path: '@/.env' })
 
 export default function Home() {
   //search bar
   const [recipes, setRecipes] = useState<any[]>([])
-  const search = async (query: string) => {
-    const response = await axios.get(process.env.RECIPE_APP_LINK + `?` + process.env.RECIPE_APP_KEY + `&query=${query}`)
-    setRecipes(response.data.results)
+  let timeout: any
+  const search = (query: string) => {
+    if (timeout) clearTimeout(timeout)
+    timeout = setTimeout(async () => {
+      const response = await axios.get(`/api/recipe?query=${query}`)
+      console.log(response)
+      if (response.data.results.length === 0) {
+        setRecipes([])
+        return
+      }
+      console.log(response.data.results)
+    }, 2000)
   }
 
   //search bar items visibility
@@ -30,11 +37,11 @@ export default function Home() {
       <div className="w-full flex flex-col items-center">
         <div className="w-full flex items-center justify-center relative">
           <Search className="text-neutral-400 absolute mr-[57%]" />
-          <Input onFocus={toggleSearch} onBlur={toggleSearch} className="w-[60%] h-12 pl-12 bg-zinc-800 border-none caret-white text-white shadow-2xl" placeholder="Search Recipes" />
+          <Input onFocus={toggleSearch} onBlur={toggleSearch} onChange={(e) => search(e.target.value)} className="w-[60%] h-12 pl-12 bg-zinc-800 border-none caret-white text-white shadow-2xl" placeholder="Search Recipes" />
         </div>
         <div className={showSearch ? "absolute top-[86px] bg-zinc-800 w-[60%] h-96 p-4 rounded-xl shadow-2xl" : "hidden"}>
           {
-            recipes.length !== 0 ?
+            recipes.length !== 0 && recipes !== undefined ?
               <div className="w-full h-full">not empty</div>
               :
               <div className="w-full h-full flex items-center justify-center"><h1 className="text-white text-2xl">Recipes not found!</h1></div>
